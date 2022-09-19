2523309877caf7265143492facfc149d084606fa
Ndubisi07 <drachyent@gmail.com>
2022-09-19 10:36:01 -0700

    my first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,15 @@
+#iclude "main.h"
+
+/**
+ * main - takes a pointer to an int as parameter
+ *updates the value it points to to 98
+ */
+
+void reset_to_98(int *n);
+{
+int i;
+i = 0;
+
+int *n = &i;
+*n = 98;
+}
