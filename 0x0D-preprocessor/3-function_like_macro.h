a7fc6612e2db247aa054e175de8931f940797202
Ndubisi07 <drachyent@gmail.com>
2022-10-10 16:33:48 -0700

    pre

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef FUNCTION_LIKE_MACRO_H
-#define function_like_macro_h
-#define ABS(x) ((x<0) ? (x) * (|-1))
+#ifndef FUNCITION_LIKE_MACRO_H
+#define FUNCITION_LIKE_MACRO_H
+#define ABS(x) ((x < 0) ? (x) * (-1) : (x))
 #endif
